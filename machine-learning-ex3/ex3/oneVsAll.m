02cb53ad65f5dcd41ea33b152a1124f2352bcdbc
Minh Quan <minhquan9607@gmail.com>
2018-12-16 20:28:21 +0200

    fix mistake in Cost Function

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -47,12 +47,10 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
-
     for c = 1:num_labels
         all_theta(c, :) = fmincg(@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
             initial_theta, options);
     end
- 
 % =========================================================================
 
 
