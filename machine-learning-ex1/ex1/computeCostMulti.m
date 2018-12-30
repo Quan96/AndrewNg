467b6e8837e2c0e28c30e846ceef796c3618d5e4
Minh Quan <minhquan9607@gmail.com>
2018-12-30 16:32:02 +0200

    remove redundancy

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -12,8 +12,9 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+h_theta = X * theta;
 
-J = (1/(2*m)) * ((X * theta - y)' * (X * theta - y)); % Vectorized
+J = 1/(2*m) * (h_theta - y)' * (h_theta - y)% Vectorized
 
 
 % =========================================================================
